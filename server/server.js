c7315da25d215a27c0336bc61c444da6c2c5cc4d
ShrikeshB <shrikeshbandivadekarpro95@gmail.com>
2024-08-13 10:20:56 +0530

    1st

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectToDB = require("./config/connection");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "hello there" });
+});
+
+const posts_controller = require("./controller/posts_controller");
+app.use("/posts", posts_controller);
+app.use("/users", posts_controller);
+
+app.listen("3001", (err) => {
+  console.log("listening at 3001..");
+});
